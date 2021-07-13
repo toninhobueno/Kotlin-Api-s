cf5d1f6bdddc1ba61a2dc48abe5fa4b4cf6e0232
tonin <antonio.leite00@hotmail.com>
2021-07-13 18:47:45 -0300

    recycler e card view finalizados e feitos alguns ajustes

--- app/src/main/java/github/toninhobueno/meuscontatos/ContactAdapter.kt
+++ app/src/main/java/github/toninhobueno/meuscontatos/ContactAdapter.kt
@@ -3,30 +3,48 @@ package github.toninhobueno.meuscontatos
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
+// Recycler view com seus construtores
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
 
     private val list: MutableList<Contact> = mutableListOf()
 
+    //Infla o layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
         return ContactAdapterViewHolder(view)
     }
 
+    //altera a posição baseado no cursor
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
         holder.bind(list[position])
     }
 
+    // tamanho da lista
     override fun getItemCount(): Int {
         return list.size
     }
 
+    //atualiza a lista
+    fun updateList(list: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
 
+    //Adapter recebe os itens do cardview
     class ContactAdapterViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(contact : Contact){
+        private  val tvName : TextView = itemView.findViewById(R.id.tv_name)
+        private  val tvPhone : TextView = itemView.findViewById(R.id.tv_phone)
+        private  val ivPhotograph : ImageView = itemView.findViewById(R.id.iv_photograph)
 
+        fun bind(contact : Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.fone
         }
     }
 }
--- app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
@@ -2,10 +2,49 @@ package github.toninhobueno.meuscontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_List)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bindView()
+        updateList()
+    }
+
+    private fun bindView() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+
+    }
+
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Antonio Carlos",
+                    "(00) 00000-0000",
+                    "img.png",
+
+                ),
+                Contact(
+                    "Antonio Carlos",
+                    "(00) 00000-0000",
+                    "img.png"
+                )
+            )
+        )
     }
 }
+
