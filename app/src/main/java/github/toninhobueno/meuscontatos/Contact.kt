6d114b98768ec5652f60da87e94248c631650f0a
tonin <antonio.leite00@hotmail.com>
2021-07-13 17:10:32 -0300

    criação do adapter e objeto contato

---
+++ app/src/main/java/github/toninhobueno/meuscontatos/Contact.kt
@@ -0,0 +1,9 @@
+package github.toninhobueno.meuscontatos
+
+data class Contact (
+
+    var name : String,
+    var fone : String,
+    var photograph : String,
+
+    )
---
+++ app/src/main/java/github/toninhobueno/meuscontatos/ContactAdapter.kt
@@ -0,0 +1,26 @@
+package github.toninhobueno.meuscontatos
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    private val list: MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    class ContactAdapterViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+}
