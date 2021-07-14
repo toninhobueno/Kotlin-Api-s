3f044d76bae781cb0d758315c7dfec77cd41fa4b
tonin <antonio.leite00@hotmail.com>
2021-07-14 19:08:45 -0300

    criado Menu Options e dois itens para testes

--- app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
@@ -2,6 +2,11 @@ package github.toninhobueno.meuscontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -45,6 +50,31 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.item_menu_1 -> {
+            showToast("Exibindo item de menu 1")
+                return true
+            }
+
+            R.id.item_menu_2 ->{
+                showToast("Exibindo item de menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
