92cd3bf3b07a1a29c26fdb00e2c02529a2329eaf
tonin <antonio.leite00@hotmail.com>
2021-07-16 18:14:32 -0300

    criado e implantado Drawer Menu

--- app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/meuscontatos/MainActivity.kt
@@ -1,14 +1,20 @@
 package github.toninhobueno.meuscontatos
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +26,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
-
+        initDrawer()
         bindView()
         updateList()
+    }
+
+
+
+    private fun initDrawer (){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+
+        val toolbar =findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer)
+
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+
     }
 
     private fun bindView() {
